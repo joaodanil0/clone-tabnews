d58187e0dce53ca5385eb9e53df8e6ae024e1eee
Joao Danilo <joaodanilo1992@gmail.com>
2025-06-04 23:33:45 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
-
-
 function Home() {
-
-
-  return <h1> Frase impactante :O</h1>
-
-
+  return <h1> Frase impactante :O</h1>;
 }
 
+function test() {
+  console.log("asdasdasd");
+}
 export default Home;
